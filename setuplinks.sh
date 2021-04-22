fba8741397d51a156f19481a45a7bb0ac2022836
code4fun907 <skylerak04@gmail.com>
2021-04-21 17:48:06 -0800

    finish dotfiles

--- setuplinks.sh
+++ setuplinks.sh
@@ -6,8 +6,13 @@ ln $HOME/dotfiles/config $HOME/.config/i3/config
 rm $HOME/.tmux.conf
 ln $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 
+mkdir $HOME/.config/alacritty
 rm $HOME/.config/alacritty/alacritty.yml
 ln $HOME/dotfiles/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
+mkdir $HOME/.config/nvim
 rm $HOME/.config/nvim/init.vim
 ln $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+
+rm $HOME/.zshrc
+ln $HOME/dotfiles/.zshrc $HOME/.zshrc
